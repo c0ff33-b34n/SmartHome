45696fdd01834ba8449db942789d7b86b52a78c9
Stuart Bowes <stuartbowes2000@gmail.com>
2021-05-27 16:43:21 +0100

    Add DataTemplateSelector for device templates

---
+++ SmartHome/DeviceTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SmartHome
+{
+    public class DeviceTemplateSelector : DataTemplateSelector
+    {
+        DataTemplate doorbellTemplate;
+        DataTemplate smokeTemplate;
+        DataTemplate thermostatTemplate;
+        public DeviceTemplateSelector()
+        {
+            doorbellTemplate = new DataTemplate(typeof(TextCell));
+            smokeTemplate = new DataTemplate(typeof(SmokeDetectorViewCell));
+            thermostatTemplate = new DataTemplate(typeof(ThermostatViewCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+
+            if (item is DoorBell)
+                return doorbellTemplate;
+
+            if (item is SmokeDetector)
+                return smokeTemplate;
+
+            if (item is Thermostat)
+                return thermostatTemplate;
+
+            throw new Exception("Could not find the device type");
+        }
+    }
+}
